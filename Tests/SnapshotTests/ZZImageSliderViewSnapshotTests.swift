4b865bde200b0e1674656c1bdfbe7deaf62c7fd2
zzmasoud <zzmasoud@gmail.com>
2023-09-13 10:56:58 -0400

    Update test plans

--- Tests/SnapshotTests/ZZImageSliderViewSnapshotTests.swift
+++ Tests/SnapshotTests/ZZImageSliderViewSnapshotTests.swift
@@ -19,7 +19,7 @@ final class ZZImageSliderViewSnapshotTests: XCTestCase {
     }
     
     func test_loadedImagesState() {
-        let (sut, loader, viewModel) = makeSUT()
+        let (sut, loader, _) = makeSUT()
         let item1 = Self.mockItems[0]
         let item2 = Self.mockItems[1]
         let item3 = Self.mockItems[2]
@@ -29,24 +29,27 @@ final class ZZImageSliderViewSnapshotTests: XCTestCase {
         loader.complete(with: .success(.make(withColor: .blue)), for: item3.id)
         
         assertSnapshot(of: sut, as: .image, record: true)
-        
-        viewModel.didTap(item: item2)
-        
-        assertSnapshot(of: sut, as: .image, record: true)
-        
-        viewModel.didTap(item: item3)
-
-        assertSnapshot(of: sut, as: .image, record: true)
     }
 
     
     // MARK: - Helpers
     
-    private func makeSUT() -> (some View, ImageLoaderStub, ZZImageSliderViewModel) {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (some View, ImageLoaderStub, ZZImageSliderViewModel) {
         let imageLoader = ImageLoaderStub()
-        let viewModel = ZZImageSliderViewModel(items: Self.mockItems, timer: TimerSpy(), delegate: nil, imageLoader: imageLoader)
+        let timer = TimerSpy()
+        let viewModel = ZZImageSliderViewModel(
+            items: Self.mockItems,
+            timer: timer,
+            delegate: nil,
+            imageLoader: imageLoader
+        )
         let sut = ZZImageSliderView(viewModel: viewModel)
             .frame(width: 370, height: 270)
+        
+        trackForMemoryLeaks(timer, file: file, line: line)
+        trackForMemoryLeaks(imageLoader, file: file, line: line)
+        trackForMemoryLeaks(viewModel, file: file, line: line)
+
         return (sut, imageLoader, viewModel)
     }
     
